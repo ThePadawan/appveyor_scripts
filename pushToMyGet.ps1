fdaf39e74912221aa3723b586dcb07b315c90052
Rudolf M. Schreier <rudolfschreier@gmx.de>
2017-03-23 22:02:05 +0100

    Add parameter validation

--- pushToMyGet.ps1
+++ pushToMyGet.ps1
@@ -1,38 +1,50 @@
-function pushToMyGet($parentFolder, $filePattern, $envKeyApiKey)
+function pushToMyGet
 {
-  Write-Host "pushToMyGet"
+  param(
+    [parameter(Mandatory=$true)]
+    [String]
+    $parentFolder,
+    [parameter(Mandatory=$true)]
+    [String]
+    $filePattern,
+    [parameter(Mandatory=$true)]
+    [String]
+    $envKeyApiKey
+  )
+  process
+  {
+    $matchingFiles = Get-ChildItem $parentFolder -recurse -Filter $filePattern
 
-  $matchingFiles = Get-ChildItem $parentFolder -recurse -Filter $filePattern
+    Write-Host "Found" $matchingFiles.Count "files matching pattern '"$filePattern"' in folder '"$parentFolder"'"
 
-  Write-Host "Found" $matchingFiles.Count "files matching pattern '"$filePattern"' in folder '"$parentFolder"'"
-
-  If ($matchingFiles.Count -eq 0) {
-    Write-Host "No files match pattern, aborting."
-    return
-  }
-
-  $matchingEnvKeys = Get-ChildItem Env: | Where-Object {$_.Name -eq $envKeyApiKey}
-
-  if ($matchingEnvKeys.Count -eq 0) {
-    Write-Host "Could not find Environment variable for MyGet feed at name" $envKeyApiKey "'"
-    return
-  }
-
-  $apiKey = $matchingEnvKeys[0].Value
-  Write-Host "Found API key with length" $apiKey.Length
-
-  $matchingFiles | Foreach-Object {
-    $directory = [io.path]::GetDirectoryName($_.FullName)
-    $fullpath = [io.path]::Combine($directory, $_)
-
-    Write-Host "Now publishing" $fullpath
-
-    $targetFeed = "https://www.myget.org/F/the_diary/api/v2/package"
-
-    If ($_ -match "\.symbols\.") {
-      $targetFeed = "https://www.myget.org/F/the_diary/symbols/api/v2/package"
+    If ($matchingFiles.Count -eq 0) {
+      Write-Host "No files match pattern, aborting."
+      return
     }
 
-    nuget push $fullpath $apiKey -Source $targetFeed
+    $matchingEnvKeys = Get-ChildItem Env: | Where-Object {$_.Name -eq $envKeyApiKey}
+
+    if ($matchingEnvKeys.Count -eq 0) {
+      Write-Host "Could not find Environment variable for MyGet feed at name" $envKeyApiKey "'"
+      return
+    }
+
+    $apiKey = $matchingEnvKeys[0].Value
+    Write-Host "Found API key with length" $apiKey.Length
+
+    $matchingFiles | Foreach-Object {
+      $directory = [io.path]::GetDirectoryName($_.FullName)
+      $fullpath = [io.path]::Combine($directory, $_)
+
+      Write-Host "Now publishing" $fullpath
+
+      $targetFeed = "https://www.myget.org/F/the_diary/api/v2/package"
+
+      If ($_ -match "\.symbols\.") {
+        $targetFeed = "https://www.myget.org/F/the_diary/symbols/api/v2/package"
+      }
+
+      nuget push $fullpath $apiKey -Source $targetFeed
+    }
   }
 }
